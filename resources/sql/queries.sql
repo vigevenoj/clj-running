be5f924838bb9a4801e4684e4e6077a68587fb40
Jacob Vigeveno <vigevenoj@gmail.com>
2018-06-12 18:11:53 -0700

    Add queries for runs, fix swagger
    Added create/update/delete methods and queries for run objects
    Updated swagger documentation so the format is right now
    Reorganized API to match what I want

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -20,6 +20,24 @@ WHERE id = :id
 DELETE FROM users
 WHERE id = :id
 
+-- :name create-run! :! :n
+-- :doc create a new run
+INSERT INTO runs
+(rdate, timeofday, distance, units, elapsed, effort, comment, shoeid)
+VALUES (:rdate, :timeofday, :distance, :units, :elapsed, :effort, :comment :shoeid)
+
+-- :name update-run! :! :n
+-- :doc update an existing run
+UPDATE runs
+SET rdate = :rdate, timeofday = :timeofday, distance = :distance,
+ units = :units, elapsed = :elapsed, effort = :effort, commment = :comment, shoeid = :shoeid
+WHERE runid = :runid
+
+-- :name delete-run! :! :n
+-- :doc deletes a run given the id
+DELETE FROM runs
+WHERE runid = :runid
+
 -- :name get-runs :? :*
 -- :doc retrieves all runs
 SELECT runid, rdate, timeofday, distance, units, elapsed, effort, comment, shoeid FROM runs
--- src/clj/running/routes/services.clj
+++ src/clj/running/routes/services.clj
@@ -9,21 +9,46 @@
              :spec "/swagger.json"
              :data {:info {:version "1.0.0"
                            :title "Running API"
-                           :description "Running Services"}}}}
+                           :description "Running Services"}
+                    :tags [{:name "runs" :description "Runs"}
+                           {:name "statistics" :description "Statistics about runs"}]}}}
   
   (context "/api/v1" []
     :tags ["An API for running data"]
+    ; This context is about runs themselves
+    (context "/runs" []
+      :tags ["runs"]
+      (GET "/" []
+        :summary "Return all runs"
+        (ok (db/get-runs)))
+      (GET "/:runid" []
+        :path-params [runid :- Long]
+        :summary "Return a run by ID"
+        (ok (db/get-run {:runid runid})))
+      ; todo need to add authentication for POST/PUT/DELTE
+      (POST "/" []
+        :summary "Add a new run"
+        (ok "not implemented"))
+      (PUT "/" []
+        :summary "Update an existing run"
+        (ok "not implemented"))
+      (DELETE "/:runid" []
+        :summary "Delete a run"
+        (ok "not implemented"))
 
-    (GET "/runs" []
-      :summary "Return all runs"
-      (ok (db/get-runs)))
-    (GET "/runs:runid" []
-      :path-params [runid :- Long]
-      :summary "Return a run by ID"
-      (ok (db/get-run runid)))
+      (GET "/bydate" []
+        :query-params [rdate]
+        :summary "Return runs that occurred on a given date"
+        (ok (db/get-by-date {:rdate rdate}))))
 
-    ; These are sligtly different and maybe belong in a different context?
-    (GET "/bydate" []
-      :query-params [rdate]
-      :summary "Return runs that occurred on a given date"
-      (ok (db/get-by-date {:rdate rdate})))))
+    ; This context is for statistics about runs
+    (context "/statistics" []
+      :tags ["statistics"]
+      (GET "/current" []
+        :path-params [period]
+        :summary "Get information about runs during current [week|month|year]"
+        (ok "not implemented"))
+      (GET "/rolling" []
+        :path-params [period]
+        :summary "Get information about runs during the past [week|month|90 days|180 days|year]"
+        (ok "not implemented")))))
