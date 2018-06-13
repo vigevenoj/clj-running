f2ee542c6db8d505ff94ec4e95dbc80d3fa8b0d0
Jacob Vigeveno <vigevenoj@gmail.com>
2018-06-12 23:11:42 -0700

    Schema and swagger improvements
    Changed TimeOfDay to s/enum, removed nil because swagger doesn't support null values
    Defined Run, RunResult (single run), and RunsResult (multiple runs) schemas
    Applied schemas to methods where applicable
    Documented some next steps

--- src/clj/running/routes/services.clj
+++ src/clj/running/routes/services.clj
@@ -4,6 +4,30 @@
             [schema.core :as s]
             [running.db.core :as db]))
 
+; define some schemas
+(def TimeOfDay (s/enum "am" "pm" "noon" "night"))
+
+(def DistanceUnits (s/enum "m" "km" "mile" "miles" "meters"))
+
+(s/defschema Run
+  {:runid s/Num
+   :rdate s/Inst
+   :timeofday TimeOfDay
+   (s/optional-key :distance) s/Num
+   (s/optional-key :units) DistanceUnits
+   (s/optional-key :elapsed) s/Any
+   (s/optional-key :comment) s/Str
+   (s/optional-key :effort) s/Str
+   (s/optional-key :shoeid) (s/maybe s/Num)})
+
+(s/defschema RunResult
+  {(s/optional-key :run) Run
+   (s/optional-key :error) s/Str})
+
+(s/defschema RunsResult
+  {(s/optional-key :runs) [Run]
+  (s/optional-key :error) s/Str})
+
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
@@ -19,29 +43,55 @@
     (context "/runs" []
       :tags ["runs"]
       (GET "/" []
+        :return RunsResult
         :summary "Return all runs"
         (ok (db/get-runs)))
       (GET "/:runid" []
+        :return RunResult
         :path-params [runid :- Long]
         :summary "Return a run by ID"
         (ok (db/get-run {:runid runid})))
       ; todo need to add authentication for POST/PUT/DELTE
       (POST "/" []
+        :body-params [rdate
+                      timeofday :- TimeOfDay
+                      distance :- s/Num
+                      units :- DistanceUnits
+                      elapsed :- s/Any
+                      comment :- s/Any
+                      effort :- s/Any
+                      shoeid :- s/Int]
+        :return s/Int
         :summary "Add a new run"
         (ok "not implemented"))
       (PUT "/" []
+        :body-params [runid :- s/Int
+                      rdate
+                      timeofday :- TimeOfDay
+                      distance :- s/Num
+                      units :- DistanceUnits
+                      elapsed :- s/Any
+                      comment :- s/Any
+                      effort :- s/Any
+                      shoeid :- s/Int]
+        :return s/Int
         :summary "Update an existing run"
         (ok "not implemented"))
       (DELETE "/:runid" []
+        :path-params [runid :- s/Int]
+        :return s/Int
         :summary "Delete a run"
         (ok "not implemented"))
 
       (GET "/bydate" []
         :query-params [rdate]
+        :return RunsResult
         :summary "Return runs that occurred on a given date"
         (ok (db/get-by-date {:rdate rdate}))))
 
     ; This context is for statistics about runs
+    ; todo: need to scope out what I want to include in the response
+    ; I think I want: total distance, units, total elapsed time, fastest pace per unit distance, avg pace per unit distance
     (context "/statistics" []
       :tags ["statistics"]
       (GET "/current" []
