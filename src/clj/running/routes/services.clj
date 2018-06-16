0392bdb09bf9ef7b0bc2531dd655e60cdf1147d6
Jacob Vigeveno <vigevenoj@gmail.com>
2018-06-16 08:36:43 -0700

    Refactor api, re-indent
    moved  /api/v1/running/runs/filter to /api/v1/running/filter
    auto-indented with whatever cursive's rules are

--- src/clj/running/routes/services.clj
+++ src/clj/running/routes/services.clj
@@ -48,74 +48,71 @@
 
   (context "/api/v1" []
     :tags ["An API for running data"]
-
+    ; /api/v1/running/
     (context "/running" []
-             ; This context is about runs themselves
-             (context "/runs" []
-               :tags ["runs"]
-               (GET "/" []
-                 :return [runs/Run]
-                 :summary "Return all runs"
-                 (runs/all-runs))
-               (GET "/bydate" []
-                 :query-params [rdate :- s/Any]
-                 :return [runs/Run]
-                 :summary "Return runs that occurred on a given date"
-                 (runs/runs-by-date rdate))
+      (GET "/bydate" []
+        :query-params [rdate :- s/Any]
+        :return [runs/Run]
+        :summary "Return runs that occurred on a given date"
+        (runs/runs-by-date rdate))
+      (GET "/filter" []
+        :query-params [{before :- (describe String "Runs before this date") ""}
+                       {after :- (describe String "Runs after this date") ""}
+                       {longerThan :- (describe s/Num "Runs longer than this") 0}
+                       {shorterThan :- (describe s/Num "Runs shorter than this") 100}
+                       {fasterThan :- (describe s/Any "Runs faster than this pace") nil}
+                       {slowerThan :- (describe s/Any "Runs slower than this pace") nil}]
+        :return runs/RunsResult
+        :summary "Returns runs that match the filters provided"
+        (ok (db/get-filtered-runs {:before-date  before
+                                   :after-date   after
+                                   :min-distance longerThan
+                                   :max-distance shorterThan})))
 
-               (GET "/:runid" []
-                 :return runs/Run
-                 :path-params [runid :- Long]
-                 :summary "Return a run by ID"
-                 (runs/run runid))
-               ; todo need to add authentication for POST/PUT/DELTE
-               (POST "/" []
-                 :body-params [rdate :- s/Any
-                               timeofday :- runs/TimeOfDay
-                               distance :- s/Num
-                               units :- runs/DistanceUnits
-                               elapsed :- s/Any
-                               comment :- s/Any
-                               effort :- s/Any
-                               shoeid :- s/Int]
-                 :return s/Int
-                 :summary "Add a new run"
-                 (not-implemented "not implemented"))
-               (PUT "/" []
-                 :body-params [runid :- s/Int
-                               rdate :- s/Any
-                               timeofday :- runs/TimeOfDay
-                               distance :- s/Num
-                               units :- runs/DistanceUnits
-                               elapsed :- s/Any
-                               comment :- s/Any
-                               effort :- s/Any
-                               shoeid :- s/Int]
-                 :return s/Int
-                 :summary "Update an existing run"
-                 (not-implemented "not implemented"))
-               (DELETE "/:runid" []
-                 :path-params [runid :- s/Int]
-                 :return s/Int
-                 :summary "Delete a run"
-                 (not-implemented "not implemented"))
-
-
-
-
-               (GET "/filter" []
-                 :query-params [{before :- (describe String "Runs before this date") ""}
-                                {after :- (describe String "Runs after this date") ""}
-                                {longerThan :- (describe s/Num "Runs longer than this") 0}
-                                {shorterThan :- (describe s/Num "Runs shorter than this") 100}
-                                {fasterThan :- (describe s/Any "Runs faster than this pace") nil}
-                                {slowerThan :- (describe s/Any "Runs slower than this pace") nil}]
-                 :return runs/RunsResult
-                 :summary "Returns runs that match the filters provided"
-                 (ok (db/get-filtered-runs {:before-date  before
-                                            :after-date   after
-                                            :min-distance longerThan
-                                            :max-distance shorterThan}))))
+      ; This context is about runs themselves
+      ; /api/v1/running/runs
+      (context "/runs" []
+        :tags ["runs"]
+        (GET "/" []
+          :return [runs/Run]
+          :summary "Return all runs"
+          (runs/all-runs))
+        (GET "/:runid" []
+          :return runs/Run
+          :path-params [runid :- Long]
+          :summary "Return a run by ID"
+          (runs/run runid))
+        ; todo need to add authentication for POST/PUT/DELTE
+        (POST "/" []
+          :body-params [rdate :- s/Any
+                        timeofday :- runs/TimeOfDay
+                        distance :- s/Num
+                        units :- runs/DistanceUnits
+                        elapsed :- s/Any
+                        comment :- s/Any
+                        effort :- s/Any
+                        shoeid :- s/Int]
+          :return s/Int
+          :summary "Add a new run"
+          (not-implemented "not implemented"))
+        (PUT "/" []
+          :body-params [runid :- s/Int
+                        rdate :- s/Any
+                        timeofday :- runs/TimeOfDay
+                        distance :- s/Num
+                        units :- runs/DistanceUnits
+                        elapsed :- s/Any
+                        comment :- s/Any
+                        effort :- s/Any
+                        shoeid :- s/Int]
+          :return s/Int
+          :summary "Update an existing run"
+          (not-implemented "not implemented"))
+        (DELETE "/:runid" []
+          :path-params [runid :- s/Int]
+          :return s/Int
+          :summary "Delete a run"
+          (not-implemented "not implemented")))
 
       ; This context is for statistics about runs
       ; todo: need to scope out what I want to include in the response
