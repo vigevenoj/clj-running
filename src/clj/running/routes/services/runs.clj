3b0295fd6c8b7fc36fe566a795ccf146e16f6b3c
Jacob Vigeveno <vigevenoj@gmail.com>
2018-06-22 14:27:13 -0700

    Style changes, add some coercion for durations
    Still not really sure how to handle coercion from duration formats into
    java.time.Duration from the client side, but at least going between
    java.time.Duration and org.postgresql.util.PGInterval works still

--- src/clj/running/routes/services/runs.clj
+++ src/clj/running/routes/services/runs.clj
@@ -4,23 +4,36 @@
             [ring.util.http-response :refer :all]
             [running.db.core :as db]
             [running.routes.services.common :refer [handler]]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [schema.coerce :as coerce])
+  (:import (java.time Duration)))
 
 ; define some schema information
 (def TimeOfDay (s/enum "am" "pm" "noon" "night"))
 (def DistanceUnits (s/enum "m" "km" "mile" "miles" "meters"))
+(def iso-duration-regex #"^P(?!$)(\d+Y)?(\d+M)?(\d+W)?(\d+D)?(T(?=\d+[HMS])(\d+H)?(\d+M)?(\d+S)?)?$")
+(def hhmmss-regex #"^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$")
+
+(defn elapsed-duration-matcher [schema]
+  (when (= java.time.Duration schema)
+    (coerce/safe
+      (fn [x]
+        (if (and (string? x) (or
+                               (re-matches iso-duration-regex x)
+                               (re-matches hhmmss-regex x)))
+                 db/string-duration-to-duration x)))))
 
 ; A schema for runs
 (s/defschema Run
-  {:runid s/Num
-   :rdate s/Any;LocalDate
-   :timeofday (s/maybe TimeOfDay)
+  {:runid                     s/Num
+   :rdate                     s/Any ;LocalDate
+   :timeofday                 (s/maybe TimeOfDay)
    (s/optional-key :distance) (s/maybe s/Num)
-   (s/optional-key :units) (s/maybe DistanceUnits)
-   (s/optional-key :elapsed) (s/maybe s/Any)
-   (s/optional-key :comment) (s/maybe s/Str)
-   (s/optional-key :effort) (s/maybe s/Str)
-   (s/optional-key :shoeid) (s/maybe s/Num)})
+   (s/optional-key :units)    (s/maybe DistanceUnits)
+   (s/optional-key :elapsed)  (s/maybe Duration)
+   (s/optional-key :comment)  (s/maybe s/Str)
+   (s/optional-key :effort)   (s/maybe s/Str)
+   (s/optional-key :shoeid)   (s/maybe s/Num)})
 
 (s/defschema RunResult
   {(s/optional-key :run) Run
@@ -48,4 +61,16 @@
 ;(handler delete-run! [runid]
 ;  (ok (db/delete-run! {:runid runid})))
 
-(handler filtered-runs [])
+(handler filtered-runs [before-date after-date min-distance max-distance]
+         (ok (db/get-filtered-runs {:before-date (c/to-sql-date :before-date)
+                                    :after-date (c/to-sql-date :after-date)
+                                    :min-distance :min-distance
+                                    :max-distance :max-distance})))
+
+;-- :snip filter-select-snip
+;select :i*:cols
+;-- :snip filter-from-snip
+; from :i*:tables
+;(filtered-query db {:select (filter-select-snip {:cols ["runid", "rdate", "timeofday", "distance", "units", "elapsed"]})
+;                    :from (filter-from-snip {:tables ["runs"]})
+;                    :where (filter-where-snip {})})
