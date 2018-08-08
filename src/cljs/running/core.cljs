a92f3253d3973e88a01e1d335114bf91eafdd70b
Jacob Vigeveno <vigevenoj@gmail.com>
2018-08-07 22:38:06 -0700

    Add duration-formatting function
    We can pretty this up later, but for now, turns ISO-8601 durations like
    "PTnHnMnS" into "nH nM nS". It shouldn't be too much work to turn that
    into two-digit hh:mm:ss if that's what I decide to do

--- src/cljs/running/core.cljs
+++ src/cljs/running/core.cljs
@@ -87,10 +87,13 @@
   ;           (c/from-date date))
   (str date))
 
-;(def reader
-;  (t/reader :json
-;            {:handlers
-;             {"LocalDate" (fn[x] )}}))
+(defn format-duration
+  "Format an ISO-8601 style duration into something more familiar"
+  [duration]
+  (let [duration-regex (re-pattern "^P(?!$)([0-9]+Y)?([0-9]+M)?([0-9]+W)?([0-9]+D)?(T(?=[0-9])([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$")]
+    ; The 6th through 9th elements are hours, minutes, and seconds.
+    ; I don't expect any of our durations to be longer than that but if they are we can test for it
+    (apply clojure.string/join " " (subvec (re-find duration-regex duration) 6 9))))
 
 (defn update-sort-value [new-val]
   (if (= new-val (:sort-val @app-state))
