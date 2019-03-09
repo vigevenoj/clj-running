d9befec5d20d70eacd2b8ea30c0d85082f9c6328
Jacob Vigeveno <vigevenoj@gmail.com>
2019-03-08 22:12:29 -0800

    Removed stale code (run-row-ui and run-display-table-ui functions)

--- src/cljs/running/views/runs.cljs
+++ src/cljs/running/views/runs.cljs
@@ -34,42 +34,6 @@
        [:input {:type "text" :placeholder "Comments"}]]
       [:button {:type :submit} "Save"]]]))
 
-(defn run-row-ui
-  "Display a single run"
-  [{:keys [runid rdate timeofday distance units elapsed comment effort] :as run}]
-  [:tr {:key runid}
-   [:td (format-date rdate)]
-   [:td timeofday]
-   [:td (if (not (nil? distance))
-          (str distance))]
-   [:td units]
-   [:td.duration (format-duration elapsed)]
-   [:td comment]
-   [:td effort]])
-
-(defn run-display-table-ui
-  "Render a table of runs"
-  [data]
-  (r/with-let [sort-order @(subscribe [:sort-value])
-               sort-ascending @(subscribe [:ascending])]
-              [:table.runningData
-               [:thead
-                [:tr
-                 [:th {:width "200" :on-click #(dispatch [:change-sort-field :rdate])
-                       } "Date"]
-                 [:th {:width "200" } "Time of Day"]
-                 [:th {:width "200" } "Distance"]
-                 [:th "Units"]
-                 [:th {:width "200" :on-click #(dispatch [:change-sort-field :elapsed])
-                       } "Elapsed"]
-                 [:th {:width "200" } "Comment"]
-                 [:th {:width "200" } "Effort"]]]
-               [:tbody
-                (when (seq data)
-                  (for [r data] ; removed [r (sorted-data data)] because we don't have a sorted-data fn
-                    ^{:key (:runid r)}
-                    [run-row-ui r]))]]))
-
 (defn run-card-ui [data]
   (let [run data] ; for now, at least
     (fn [run]
