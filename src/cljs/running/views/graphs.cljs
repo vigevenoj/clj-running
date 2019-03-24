ab9d20f8587ddce4bdd592e34951432b0bc792ea
Jacob Vigeveno <vigevenoj@gmail.com>
2019-03-24 09:59:34 -0700

    WIP heatmap: generate a full year and add day rects to svg for it
    had to re-work x-offset to take into account difference between ISO weekyear and the standard year (ie, week 53 of year x is week 1 of year x+1)
    added methods to find first and last year in a dataset (naively, does substring instead of parsing them as dates)
    utility method for removing the viz svg that I am testing with (should be removed before shipping, probably)
    full-year-iterate is where the meat is: it generates an svg for a given year (hardcoded to 2019 right now) and then adds day rects to the svg
    
    important fix: dereference the subscription once and pass that around instead of dereferencing it repeatedly

--- src/cljs/running/views/graphs.cljs
+++ src/cljs/running/views/graphs.cljs
@@ -13,13 +13,19 @@
 
 ;  data is fetched and lives in app-db's :heatmap-data key
 ; equivalent to index.html's d3.range(2003, 2019) call is something like
-; (-> js/d3 (.range 2003 2019)) to produce a js array [2003 2004 ... 2018]
+; (-> js/d3 (.range 2003 2020)) to produce a js array [2003 2004 ... 2019]
+
+
+; this gives a selection that contains something maybe useful, (it's rather long)
+; (.datum (.attr (.attr (.attr (.attr (.attr (.append (.enter (.data (js/d3.selectAll ".day") (fn [d] (graphs/generate-dates-for-year d)))) "rect") "class" "day") "width" 16) "height" 16) "x" (fn [d] (graphs/offset-x d))) "y" (fn [d] (graphs/offset-y d))) (js/d3.timeFormat "%Y-%m-%d"))
+; but i don't know how to use that selection
 
 ;
 (def year-height 136) ; Height of the svg container for a year
 (def year-width 900) ; Width of the svg container for a year
 (def cell-size 16) ; Cell for each day is this big
 (def date-format (format/formatters :date)) ; YYYY-MM-dd
+(def formatTime (.timeFormat js/d3 "%Y-%m-%d")) ; convert from js Date to YYYY-MM-dd
 
 (defn generate-dates-for-year
   "Generate a js array of dates for an entire year"
@@ -27,18 +33,19 @@
   (js/d3.timeDays (js/Date. year 0 1) (js/Date. (+ year 1) 0 1)))
 
 (defn year-range
-  "Generate a range of years"
+  "Generate a range of years. Begin is inclusive, end is exclusive."
   [begin end]
   (-> js/d3 (.range begin end)))
 
 ; this is the equivalent of lines 83-86-ish:
-(defn ugly-svg []
+; (ugly-svg (year-range 2003 2020))
+(defn create-svg-containers-for-years [years]
   (.attr
     (.append
       (.enter
         (.data
           (js/d3.selectAll "svg" (js/d3.select "body"))
-          (year-range 2003 2019))) "svg")
+          years)) "svg")
     "class" "Blues")
   ; this then adds the correct attributes to the svgs with class "Blues"
   (-> (.filter (js/d3.selectAll "svg") ".Blues")
@@ -54,8 +61,11 @@
 (defn offset-x
   "Calculate x-offset (horizontal) for a given day. This is based on the week in which the day is found."
   [date-string]
-  (let [date (format/parse date-format date-string)]
-    (* cell-size (format/unparse (cljs-time.format/formatter "w") date))))
+  (let [date (format/parse date-format date-string)
+        weekyear (format/unparse (cljs-time.format/formatter "x") date)
+        year (format/unparse (cljs-time.format/formatter "Y") date)
+        year-diff (- weekyear year)]
+    (+ (* cell-size (format/unparse (cljs-time.format/formatter "w") date)) (* 52 cell-size year-diff))))
 
 (defn offset-y
   "Calculate y-offset (vertical) for a given day. This is based on which day of the week the day is."
@@ -69,12 +79,60 @@
   [elements]
   (apply max (map :distance elements)))
 
+(defn find-first-year
+  "Return first year (yyyy) in the results"
+  [elements]
+  ; todo probably worth the cost of parsing/unparsing dates here to be sure about this
+  (subs (str (apply min (map :rdate elements))) 0 4))
+
+(defn find-last-year
+  "Return the last year in the results"
+  [elements]
+  ; todo probably worth the cost of parsing/unparsing dates here to be sure about this
+  (subs (str (apply max (map :rdate elements))) 0 4))
+
 (def color
   (-> js/d3 .scaleQuantize ; todo domain should use maximum distance function above
       (.domain #js [0 45]) ; Domain: our runs are 0-45 miles long (todo adjust for km)
       (.range              ; define our output range
         (.map (.range js/d3 6) ; output range has six values: "q0-6" "q1-6" "q2-6" "q3-6" "q4-6" "q5-6"
               (fn [distance] (str "q" distance "-6")))))) ; these are the blue colors defined in the original js
+
+; This method is used in testing to remove the svg from the viz element to try out different things
+(defn remove-viz-svg []
+  (-> js/d3
+      (.selectAll "#viz svg")
+      (.remove)))
+
+(defn full-year-iterate []
+  (let [days (generate-dates-for-year 2019)]
+    (.log js/console "There are " (count days) "elements")
+    (let [node (js/d3.select "#viz")]
+      (.log js/console "adding svg to #viz element")
+      (-> node
+          (.append "svg")
+          (.attr "height" year-height)
+          (.attr "width" year-width))
+      ; what i want here is for the days data to be appended to the selection
+      ; so i can append them all as rects to the svg
+      (let [svg-element (js/d3.select "#viz svg")]
+        (.log js/console svg-element)
+        (goog.object/forEach days
+                             (fn [day]
+                               (let [formatted-day (-> day formatTime)]
+                                 (.log js/console "writing node for " formatted-day)
+                                 (-> svg-element
+                                     (.append "rect")
+                                     (.attr "class" "day")
+                                     (.attr "height" cell-size)
+                                     (.attr "width" cell-size)
+                                     (.attr "x" (fn [d] (offset-x formatted-day)))
+                                     (.attr "y" (fn [d] (offset-y formatted-day)))
+                                     (.attr "fill" "#fff")
+                                     (.attr "stroke" "#ccc")
+                                     (.text formatted-day)))))
+        ))))
+
 
 (defn day-cell-did-mount
   [node ratom]
@@ -103,7 +161,7 @@
 (defn bleep-boop
   "this is a logging method and will be removed when i'm done"
   []
-  (let [data (subscribe [::subscriptions/heatmap-data])]
+  (let [data @(subscribe [::subscriptions/heatmap-data])]
     (.log js/console "there are " (count (get data :dataset)) " items in heatmap-data dataset")
     (when (not (empty? (get data :dataset)))
       (.log js/console "max in data is " (find-max-distance (get data :dataset))))))
@@ -129,5 +187,6 @@
                     :did-mount day-cell-did-mount}]}]))))
 
 (defn graph-page []
-  [:div "imagine a graph"]
-  [heatmap])
+  [:div#viz "imagine a graph"]
+;  [heatmap]
+  )
