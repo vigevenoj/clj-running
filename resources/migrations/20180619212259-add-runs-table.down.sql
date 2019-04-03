2d71358ba81bc1a7bcbcb5485e7ae2d8b6dcb0a3
Jacob Vigeveno <vigevenoj@gmail.com>
2019-04-02 21:57:33 -0700

    turns out reading the docs for the migration framework is a good idea
    migrations work right
    testing run db functionality; test is brittle because it assumes that the primary key sequence is at its initial value (1) instead of working even if the tests have been run before

--- resources/migrations/20180619212259-add-runs-table.down.sql
+++ resources/migrations/20180619212259-add-runs-table.down.sql
@@ -1,3 +1,5 @@
 drop table unit_conversion;
-drop sequence runs_runid_seq;
+--;;
 drop table runs;
+--;;
+drop sequence runs_runid_seq;
--- resources/migrations/20180619212259-add-runs-table.up.sql
+++ resources/migrations/20180619212259-add-runs-table.up.sql
@@ -3,14 +3,32 @@ CREATE TABLE unit_conversion (
   to_u varchar(5) default NULL,
   factor double precision default NULL
 ) ;
-
+--;;
+INSERT INTO unit_conversion VALUES ('m','km',0.001);
+--;;
+INSERT INTO unit_conversion VALUES ('km','m',1000);
+--;;
+INSERT INTO unit_conversion VALUES ('miles','km',1.609344);
+--;;
+INSERT INTO unit_conversion VALUES ('miles','m',1609.344);
+--;;
+INSERT INTO unit_conversion VALUES ('km','miles',0.621371192);
+--;;
+INSERT INTO unit_conversion VALUES ('m','miles',0.000621371192);
+--;;
+INSERT INTO unit_conversion VALUES ('miles','miles',1);
+--;;
+INSERT INTO unit_conversion VALUES ('km','km',1);
+--;;
+INSERT INTO unit_conversion VALUES ('m','m',1);
+--;;
 CREATE SEQUENCE public.runs_runid_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
+--;;
 CREATE TABLE public.runs (
     runid bigint default nextval('runs_runid_seq') not null,
     rdate date,
